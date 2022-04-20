b25ab3f08b3d13ea903fe751d000279749e4749e
Sebastian McKay <stmckay@umass.edu>
2022-04-20 14:41:25 -0400

    Finised implementing myUser page and added some styling

--- myuser-page/myUser.js
+++ myuser-page/myUser.js
@@ -5,27 +5,27 @@
 // }
 const clubs = document.getElementById("clubs-list");
 const friends = document.getElementById("friends-list");
-const response = {
-    "clubs": ["bob", "yuval", "people"],
-    "friends": ["cap","doodlebob"]
-}
-// ADD THIS BACK IN LATER
-// const url = new URL("milfs.com/get-fields");
-// async function init(){
-//     if(localStorage.getItem("accset") === null){
-//         location.href="../account/signin.html"
-//     }
-//     else{
-//         let response = await fetch(url, {
-//             method: "POST",
-//             body: {
-//                 user: localStorage.getItem("user"),
-//                 pass: localStorage.getItem("pass"),
-//             }
-//         });
-//         response = response.json();
-//     }   
+// const response = {
+//     "clubs": ["bob", "yuval", "people"],
+//     "friends": ["cap","doodlebob"]
 // }
+// ADD THIS BACK IN LATER
+const url = new URL("milfs.com/get-fields");
+async function init(){
+    if(localStorage.getItem("accset") === null){
+        location.href="milfs.com/signin.html"
+    }
+    else{
+        let response = await fetch(url, {
+            method: "POST",
+            body: {
+                user: localStorage.getItem("user"),
+                pass: localStorage.getItem("pass"),
+            }
+        });
+        response = response.json();
+    }   
+}
 
 for(let i=0; i< response['clubs'].length; i++){
     const newListElement= document.createElement("li");
@@ -34,8 +34,10 @@ for(let i=0; i< response['clubs'].length; i++){
     clubs.appendChild(newListElement);
     const specificClub = document.getElementById(response['clubs'][i]);
     specificClub.innerHTML = response['clubs'][i];
-    specificClub.addEventListener("click", (e)=> {
-        window.location.href = ("../club-page/club-page.html");
+    specificClub.addEventListener("click", async (e)=> {
+        const urlClub = new URL("milfs.com/get-club");
+        urlClub.searchParams.append("club", specificClub);
+        let responseClub = await fetch(urlClub, { method: 'GET', redirect: 'follow' });
     });
 }
 for(let i=0; i< response['friends'].length; i++){
